41a33d101b09072e46aca0bad4b03523f1063d65
Brad Rossman <brad5131@gmail.com>
2014-05-12 07:10:17 -0400

    Stylesheet and Contact us page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about" #creates about path
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
+
+   #creates about path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
