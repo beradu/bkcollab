53d5a1662f0d2b18e6058dae5577d3ce48327ef0
Brad Rossman <brad5131@gmail.com>
2014-06-08 18:00:21 -0400

    Fixed Table error for Ids

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607224509) do
+ActiveRecord::Schema.define(version: 20140608161759) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
