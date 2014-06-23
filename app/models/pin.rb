ac6459e4ed9805272cbab9802dbf71a745d06d8f
Brad Rossman <brad5131@gmail.com>
2014-06-23 07:50:39 -0400

    Added names to db and to pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,9 @@ class Pin < ActiveRecord::Base
 	belongs_to :user 
 	has_attached_file :image, :styles => {:full => "auto", :large => "600x600>", :medium => "300x300>", :thumb => "100x100>"}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates :image, presence: true
+	validates :description, presence: true
+
 end
 
 
