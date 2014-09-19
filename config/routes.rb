aac143a7e22b28600539f00d1d6e9cedab5dcd08
Brad Rossman <brad5131@gmail.com>
2014-09-19 07:01:47 -0400

    bundle install

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Pinteresting::Application.routes.draw do
 
   devise_for :users
   root "pins#index"
-  get "about" => "pages#about"
   get "Pins" => "pages#pins"
   get "ideas" => "pages#ideas"
+  get "about" => "pages#about"
 
-   #creates about path
+  #creates about path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
